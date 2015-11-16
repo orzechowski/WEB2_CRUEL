2b9f26471f5df1a16876442443c174b953b785f2
orzechowski <alinedaisy@gmail.com>
2015-11-16 20:04:24 -0200

    classe ingrediente, diagrama de caso de uso e descricao dos casos de uso

--- CRUEL/src/java/Ingrediente.java
+++ CRUEL/src/java/Ingrediente.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,32 @@
  *
  * @author orzechowski
  */
-public class Ingrediente {
+public class Ingrediente implements Serializable{
+    
+    private int id_ingrediente;
+    private String nome;
+    private String descricao;
+    private tipoIngrediente tipoIngrediente;
+    public Ingrediente(){
+   
+    }
+    public int getId_ingrediente(){
+        return this.id_ingrediente;
+    }
+    public void setId_ingrediente(int id_ingrediente){
+        this.id_ingrediente =  id_ingrediente;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
     
 }
