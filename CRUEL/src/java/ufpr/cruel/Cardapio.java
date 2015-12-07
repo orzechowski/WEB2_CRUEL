07a5c88df02875c82af163046df9b328265117cc
kz3r <cristopherz@gmail.com>
2015-12-06 23:56:21 -0200

    Fix Classe Cardapio e Criação daoCardapio
    [Add]Lista de Ingredientes no cardápio
    Criado daoCardapio mas completamente NÃO FUNCIONAL ainda

--- CRUEL/src/java/ufpr/cruel/Cardapio.java
+++ CRUEL/src/java/ufpr/cruel/Cardapio.java
@@ -1,11 +1,12 @@
-
-import java.io.Serializable;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ufpr.cruel;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  *
@@ -15,6 +16,7 @@ public class Cardapio implements Serializable{
     private int idCardapio;
     private String data;
     private int refeicao;
+    private List<Ingrediente> listaIngredientes;
     
     Cardapio(){    
     }
@@ -41,6 +43,16 @@ public class Cardapio implements Serializable{
             return "Almoco";
         else
            return "Jantar";
-        
+    }
+    private void setListaIngredientes(List<Ingrediente> listaIng){
+        this.listaIngredientes = listaIng;
+    }
+    
+    private void addIngrediente(Ingrediente ing){
+        this.listaIngredientes.add(ing);
+    }
+    
+    private List<Ingrediente> getListaIngredientes(){
+        return this.listaIngredientes;
     }
 }
